50c8cabe88415e465c94e004de795fb6d2c06977
vijay <vijay@192.168.100.4>
2019-05-25 22:56:18 +0530

    binary search algorithm

--- src/datastructures/BinarySearch.java
+++ src/datastructures/BinarySearch.java
@@ -6,26 +6,30 @@ import java.util.Scanner;
 public class BinarySearch {
 
 	public static void main(String[] args) {
-		int a[] = {1,43,32,67,89,49,75,51,61,8,40,100};
+		int a[] = {1,43,32,67,89,49,75,51,8,40,100};
 		Scanner s = new Scanner(System.in);
 		int ele = s.nextInt();
 		Arrays.sort(a);
-		System.out.println(Arrays.binarySearch(a, 74));
-		/*int firstIndex = 0;
-		int lastIndex = a.length;
-		int middleIndex = a.length/2;
-		Arrays.sort(a);
-		while(true) {
-			System.out.println(Arrays.toString(a)+" "+middleIndex);
+		//System.out.println(Arrays.binarySearch(a, 74));
+		binarySearch(a,ele);
+		/**/
+	}
+
+	private static void binarySearch(int[] a, int ele) {
+		int low=0;
+		int high = a.length-1;
+		System.out.println(Arrays.toString(a));
+		while(low<=high) {
+			int middleIndex = (low+high)/2;
 			if(a[middleIndex] == ele) {
 				System.out.println(middleIndex);
 				break;
 			} else if(a[middleIndex] < ele){
-				middleIndex = (middleIndex+lastIndex)/2;
+				low = middleIndex+1;
 			} else if(a[middleIndex] > ele) {
-				middleIndex = middleIndex/2;
+				high = middleIndex-1;
 			}
-		}*/
+		}
 	}
 
 }
