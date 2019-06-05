e8d3df7502579c779f318743ccf1e4776841bdbd
vijay <vijay@vijays-Mac-mini.local>
2019-06-05 18:56:06 +0530

    custom exceptions

---
+++ src/Exceptions/GreaterThanTenException.java
@@ -0,0 +1,33 @@
+package Exceptions;
+
+public class GreaterThanTenException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GreaterThanTenException() {
+	}
+
+	public GreaterThanTenException(String message) {
+		super(message);
+	}
+
+	public GreaterThanTenException(Throwable cause) {
+		super(cause);
+		
+	}
+
+	public GreaterThanTenException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	public GreaterThanTenException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		
+	}
+
+}
